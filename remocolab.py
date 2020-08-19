59432f3c8d86427aafef7e973d39a12a5971a8be
dat1063 <35129233+dat1063@users.noreply.github.com>
2020-08-19 20:50:48 +0700

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -204,7 +204,7 @@ def _setupSSHDMain(ngrok_region, check_gpu_available, is_VNC):
     print("sa - South America (Sao Paulo)")
     print("jp - Japan (Tokyo)")
     print("in - India (Mumbai)")
-    ngrok_region = region = input()
+    ngrok_region = region = in
 
   return (True, _setupSSHDImpl(ngrok_token, ngrok_region, is_VNC))
 
